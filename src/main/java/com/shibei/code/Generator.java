d83f1af8ef69dc3fce867ddcb591ab21e51723cd
linminna <linminna@52wana.com>
2019-06-10 15:24:55 +0800

    first commit

---
+++ src/main/java/com/shibei/code/Generator.java
@@ -0,0 +1,47 @@
+package com.shibei.code;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.out;
+
+/**
+ * Created by dongkun1 on 2017/3/14.
+ */
+public class Generator {
+
+    public static void main(String[] args) {
+        try {
+            List<String> warnings = new ArrayList<>();
+            boolean overwrite = true;
+            File configFile = new File("src/main/resources/generatorConfig-gameConfig-old.xml");
+            out.println(configFile.getAbsolutePath());
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+            System.out.println("---excute success---");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+    }
+}
